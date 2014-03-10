e2db86d619dab3f76fcb490f87bf3913adfebfce
David Zammit <projektzaustralia@gmail.com>
2014-03-10 14:46:07 +1100

    Update main.cpp
    added menus to go through, cant get my menu switch to work.

--- main.cpp
+++ main.cpp
@@ -1,58 +1,115 @@
 // Hero's Inventory 3.0
 // Demonstrates iterators
-
 #include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-int main()
+void weapons()
 {
-    vector<string> inventory;
-    inventory.push_back("sword");
-    inventory.push_back("armor");
-    inventory.push_back("shield");
-
     vector<string>::iterator myIterator;
     vector<string>::const_iterator iter;
 
-    cout << "Your items:\n";
-    for (iter = inventory.begin(); iter != inventory.end(); ++iter)
+    vector<string> weapons;
+    weapons.push_back("longsword");
+    weapons.push_back("shortsword");
+    weapons.push_back("dagger");
+    weapons.push_back("dagger");
+    weapons.push_back("light crossbow");
+
+    cout << "Your Weapons:\n\n";
+    for (iter = weapons.begin(); iter != weapons.end(); ++iter)
 	{
         cout << *iter << endl;
 	}
+}
 
-    cout << "\nYou trade your sword for a battle axe.";
-    myIterator = inventory.begin();
-    *myIterator = "battle axe";
-    cout << "\nYour items:\n";
-    for (iter = inventory.begin(); iter != inventory.end(); ++iter)
+void armor()
+{
+    vector<string>::iterator myIterator;
+    vector<string>::const_iterator iter;
+
+    vector<string> armor;
+    armor.push_back("chainmail");
+    armor.push_back("wooden shield");
+    armor.push_back("helmet");
+    armor.push_back("bracers");
+
+    cout << "\n\nYour Armor:\n\n";
+    for (iter = armor.begin(); iter != armor.end(); ++iter)
 	{
         cout << *iter << endl;
 	}
+}
 
-    cout << "\nThe item name '" << *myIterator << "' has ";
-    cout << (*myIterator).size() << " letters in it.\n";
+void items()
+{
+    vector<string>::iterator myIterator;
+    vector<string>::const_iterator iter;
 
-    cout << "\nThe item name '" << *myIterator << "' has ";
-    cout << myIterator->size() << " letters in it.\n";
+    vector<string> items;
+    items.push_back("potion");
+    items.push_back("potion");
+    items.push_back("caltrops");
+    items.push_back("torch");
+    items.push_back("rope");
+    items.push_back("winter fur");
+    items.push_back("sharpening stone");
 
-    cout << "\nYou recover a crossbow from a slain enemy.";
-    inventory.insert(inventory.begin(), "crossbow");
-    cout << "\nYour items:\n";
-    for (iter = inventory.begin(); iter != inventory.end(); ++iter)
+    cout << "\n\nYour Items:\n\n";
+    for (iter = items.begin(); iter != items.end(); ++iter)
 	{
         cout << *iter << endl;
 	}
+}
+void itemList()
+{
+    cout << "What would you like to see?" << endl;
+    cout << "1. Weapons\n";
+    cout << "2. Armor\n";
+    cout << "3. Items\n";
+    cout << "4. All\n";
+    char menu;
+    cin >> menu;
 
-    cout << "\nYour armor is destroyed in a fierce battle.";
-    inventory.erase((inventory.begin() + 2));
-    cout << "\nYour items:\n";
-    for (iter = inventory.begin(); iter != inventory.end(); ++iter)
-	{
-        cout << *iter << endl;
-	}
+    switch(menu)
+    {
+        case 1:
+            weapons();
+            break;
+        case 2:
+            armor();
+            break;
+        case 3:
+            items();
+            break;
+        default:
+            cout << "Please select 1-3." << endl;
+    }
+}
+int main()
+{
+    cout << "Welcome to Hero Inventory 1.0!\n";
+    cout << "Created by David Zammit\n\n";
+    cout << "This application is designed to help you keep track of your characters inventory by allowing you to navigate using type. You will be able to add items and remove items as well as manage your money.\n" << endl;
 
-	return 0;
+    cout << "Would you like to begin?" << endl;
+    char ans;
+    cin >> ans;
+    switch(ans)
+    {
+        case 'y':
+            cout << "Loading...\n\n" << endl;
+            itemList();
+            break;
+        case 'n':
+            cout << "Thank You!" << endl;
+            return 0;
+            break;
+        default:
+            cout << "Please select 'y' or 'n' to continue." << endl;
+    }
+
+    return 0;
 }
